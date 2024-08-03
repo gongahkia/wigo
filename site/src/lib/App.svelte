04198d5a106185a54d3ac80cd260e02347000053
gongahkia <angryapplegravy@gmail.com>
2024-08-03 20:39:01 +0800

    added some files

---
+++ site/src/lib/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Piece from './Piece.svelte';
+
+  let pieces = [
+    { name: 'white_pawn', ability: 'Moves forward one square' },
+    { name: 'black_rook', ability: 'Moves horizontally or vertically' },
+    // Add more pieces as needed
+  ];
+
+  let selectedPiece = null;
+
+  function handlePieceClick(piece) {
+    selectedPiece = piece;
+  }
+</script>
+
+<main>
+  <div class="chessboard">
+    {#each pieces as piece (piece.name)}
+      <Piece piece={piece} onClick={() => handlePieceClick(piece)} />
+    {/each}
+  </div>
+
+  {#if selectedPiece}
+    <div class="info">
+      <h2>Selected Piece</h2>
+      <Piece piece={selectedPiece} />
+    </div>
+  {/if}
+</main>
+
+<style>
+  .chessboard {
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(8, 1fr);
+    gap: 1px;
+    background-image: url('/chessboard.png');
+    background-size: cover;
+    width: 400px;
+    height: 400px;
+  }
+
+  .info {
+    margin-top: 20px;
+  }
+</style>
---
+++ site/src/lib/Piece.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let piece;
+  export let onClick;
+
+  const { name, ability } = piece;
+</script>
+
+<div class="piece" on:click={onClick}>
+  <img src={`/pieces/${name}.png`} alt={name} />
+  <div class="description">
+    <strong>{name}</strong>
+    <p>{ability}</p>
+  </div>
+</div>
+
+<style>
+  .piece {
+    cursor: pointer;
+    text-align: center;
+  }
+
+  .piece img {
+    width: 50px;
+    height: 50px;
+  }
+
+  .description {
+    margin-top: 5px;
+  }
+</style>
